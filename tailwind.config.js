cec927f6c66765a27060c7e366886f25a21487ea
drendog <53359960+drendog@users.noreply.github.com>
2022-07-08 12:48:13 +0200

    chore(tailwind): add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.pink,
+      }
+    },
   },
   plugins: [],
   safelist: ['w-1/12', 'w-2/12', 'w-3/12', 'w-4/12', 'w-5/12', 'w-6/12', 'w-7/12', 'w-8/12', 'w-9/12', 'w-10/12', 'w-11/12', 'w-12/12',
