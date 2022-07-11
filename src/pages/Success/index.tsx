d5ce2c29e84547622efc12b5419f4cd334eb0ed2
drendog <53359960+drendog@users.noreply.github.com>
2022-07-11 15:42:30 +0200

    refactor(success): remove extracted components and import them

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,77 +1,33 @@
-import {
-  useCallback, useEffect, useMemo, useState,
-} from 'react';
-import { fetchUsers } from '../../api/registration';
-import Alert from '../../components/Alert';
+import { useCallback, useState } from 'react';
 import Col from '../../components/layout/Col';
 import Container from '../../components/layout/Container';
 import Row from '../../components/layout/Row';
-import { UserListType } from '../../types';
+import UserOperation from '../../components/UserOperation';
+import UserTable from '../../components/UserTable';
 
 const Success = (): JSX.Element => {
-  const [userList, setUserList] = useState<UserListType>();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>();
+  const [updateState, setUpdateState] = useState<number>(0);
 
-  const getUserList = useCallback(async () => {
-    if (!isLoading) return;
-    await fetchUsers().then((res) => {
-      setUserList(res);
-    }).catch((err: any) => {
-      setErrorMessage(err.message);
-    }).finally(() => {
-      setIsLoading(false);
-    });
-  }, [isLoading]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    if (!userList) {
-      getUserList();
-    }
-  }, [getUserList, userList]);
-
-  const users = useMemo(
-    () => {
-      if (!userList) return <></>;
-      // table
-      return userList.map((user) => (
-        <tr key={user.id}>
-          <td>{user.id}</td>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>{user.status}</td>
-        </tr>
-      ));
-    },
-    [userList],
-  );
+  const updateUsers = useCallback(() => {
+    setUpdateState(updateState + 1);
+    console.log(updateState);
+  }, [updateState]);
 
   return (
     <Container>
       <Row>
         <Col sm={12}>
-          <h1>Successful</h1>
+          <UserTable title="Success!" key={updateState} />
         </Col>
       </Row>
       <Row>
         <Col sm={12}>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            {users}
-          </table>
+          <UserOperation operation="get" />
         </Col>
       </Row>
       <Row>
         <Col sm={12}>
-          <Alert message={errorMessage} show={!!errorMessage} />
+          <UserOperation onChange={updateUsers} operation="delete" />
         </Col>
       </Row>
     </Container>
