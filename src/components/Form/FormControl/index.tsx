d22567f63602cf9c5108790a2c164f73efe2d689
drendog <53359960+drendog@users.noreply.github.com>
2022-07-11 12:57:30 +0200

    chore(form): add validate prop to form-control

--- src/components/Form/FormControl/index.tsx
+++ src/components/Form/FormControl/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Field } from 'formik';
 import Container from '../../layout/Container';
 import Row from '../../layout/Row';
 import Col from '../../layout/Col';
+import FormMessageError from '../FormMessageError';
 
 interface IFormControlProps {
   name: string;
@@ -10,6 +11,7 @@ interface IFormControlProps {
   placeholder?: string;
   isValid?: boolean;
   errorMessage?: string;
+  validate?: (value: string) => boolean;
 }
 
 const FormControl = ({
@@ -18,24 +20,29 @@ const FormControl = ({
   placeholder,
   isValid,
   errorMessage,
-}: IFormControlProps): JSX.Element => (
-  <Container className="py-2">
-    <Row>
-      <Col sm={12}>
-        <Field
-          name={name}
-          type={type}
-          placeholder={placeholder}
-          className={`w-full appearance-none rounded border-0 border-b-2 bg-gray-50 py-2 px-3 leading-tight text-gray-700 hover:bg-gray-100 focus:border-primary-500 focus:bg-gray-100 focus:outline-none ${isValid === false && 'border-red-500'}`}
-        />
-      </Col>
-    </Row>
-    <Row>
-      <Col sm={12}>
-        <p>{isValid === false && errorMessage}</p>
-      </Col>
-    </Row>
-  </Container>
-);
+  validate,
+}: IFormControlProps): JSX.Element => {
+  const messageError = useMemo(
+    () => (!isValid && errorMessage ? <FormMessageError errorMessage={errorMessage} /> : <></>),
+    [isValid, errorMessage],
+  );
+
+  return (
+    <Container className="py-2">
+      <Row>
+        <Col sm={12}>
+          <Field
+            name={name}
+            type={type}
+            placeholder={placeholder}
+            validate={validate}
+            className={`w-full appearance-none rounded border-0 border-b-2 bg-gray-50 py-2 px-3 leading-tight text-gray-700 hover:bg-gray-100 focus:border-primary-500 focus:bg-gray-100 focus:outline-none ${isValid === false && 'border-red-500'}`}
+          />
+        </Col>
+      </Row>
+      {messageError}
+    </Container>
+  );
+};
 
 export default FormControl;
